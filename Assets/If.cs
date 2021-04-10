81d9c0ebbaa9adc8b07f335b0338334e62af9403
D4WR1N <80801467+D4WR1N@users.noreply.github.com>
2021-04-09 19:08:52 -0500

    Initial commit

---
+++ Assets/If.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class If : MonoBehaviour
+{
+    public float TemperaturaDelCafe = 63f;
+    float LimiteCalienteDeLaTemperatura = 80f;
+    float LimiteFrioDeLaTemperatura = 20f;
+
+    void Update()
+    {
+        if (TemperaturaDelCafe > LimiteCalienteDeLaTemperatura)
+        {
+            print("We te mamaste, te vas a quemar");
+        }
+        if (TemperaturaDelCafe < LimiteFrioDeLaTemperatura)
+        {
+            print("Buah tomas un poco y se te congela el cerebro");
+        }
+        else
+        {
+            print("Perfectamente balanceado como debe de estar");
+        }
+
+        
+    }
+}
---
+++ Assets/Lopeacion.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lopeacion : MonoBehaviour
+{
+    int numeroDeArañas = 3;
+
+
+    void Start()
+    {
+        for (int i = 0; i < numeroDeArañas; i++)
+        {
+            print("Son bastantes arañas, oh con esta van " + i);
+        }
+
+    }
+}
+
+
